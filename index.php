67c6cb7315fd4956f01c7cdccedbe7c11d416436
Lớp C2110I1 <c2110i1sql@gmail.com>
2022-11-07 15:30:25 +0700

    Request form & Upload

--- index.php
+++ index.php
@@ -1,4 +1,48 @@
-<?php include 'header.php';?>
+<?php include 'header.php';
+$allow_types = ['image/jpeg','image/jpg','image/png','image/gif'];
+
+// if ( isset($_POST['name']) ) {
+//     $name = $_POST['name'];
+//     $status = $_POST['status'];
+//     $sql = "INSERT INTO category (name, status) VALUES('$name','$status')";
+//     echo $sql;
+// }
+$name = '';
+if (!empty($_FILES['upload']['tmp_name'])) {
+    $file = $_FILES['upload'];
+    $type = $file['type'];
+    if (in_array($type, $allow_types)) {
+        $tmp_name = $file['tmp_name'];
+        $name = $file['name'];
+        move_uploaded_file($tmp_name, 'uploads/'.$name);
+    } else {
+        echo 'Định dạng file hợp lệ là: jpg, png, gif';
+    }
+   
+}
+echo '<pre>';
+    print_r($_FILES);
+echo '</pre>';
+
+?>
+
+<div class="container">
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <legend>Form Upload file</legend>
+
+    <div class="form-group">
+        <label for="">Chọn file</label>
+        <input type="file" class="form-control" 
+            name="upload" >
+    </div>
+    <img src="uploads/<?=$name;?>" alt="" width="240">
+    <button type="submit" 
+        class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+
 
     <div id="carouselId" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
